299ae5ed774a47cf64d15bbdb602a8535a31f0e8
Matias Sothers <Matias.sothers@ug.uchile.cl>
2023-10-20 17:07:51 -0300

    Co-authored-by: shejonoma <shejonoma@users.noreply.github.com> Co-authored-by: Javier González Bustos <Javiergonb@users.noreply.github.com>

---
+++ codigo_rasp/client.py
@@ -0,0 +1,75 @@
+from bleak import BleakScanner
+from modelos import create_tables
+from modelos import Configuration
+from peewee import DoesNotExist
+
+TAG = "[RASP]"
+
+print(TAG, "Creando tablas")
+create_tables()
+
+class Config:
+    def __init__(self, transport_layer=0, id_protocol=0):
+        self.transport_layer = transport_layer
+        self.id_protocol = id_protocol
+        self.config = Configuration()
+        self.row = None
+        try:
+            self.row = self.config.get_by_id(1)
+        except DoesNotExist:
+            default_row = {"transport_layer": self.transport_layer, "id_protocol": self.id_protocol}
+            self.config.create(**default_row)
+            self.row = self.config.get_by_id(1)
+
+    def get(self):
+        self.row = self.config.get_by_id(1)
+        return (self.row.transport_layer, self.row.id_protocol)
+    
+    def set(self, transport_layer, id_protocol):
+        self.row.transport_layer = transport_layer
+        self.row.id_protocol = id_protocol
+        self.row.save()
+
+
+class ClientHandler:
+    def __init__(self):
+        self.scanner = BleakScanner()
+        self.client = None
+        self.config = Config()
+
+    def get_config(self):
+        return self.config.get()
+
+    async def discover(self):
+        devices = await self.scanner.discover()
+        return devices
+    
+    async def connect(self, device_mac):
+        self.client = BleakScanner()
+        connected = self.client.connect(device_mac)
+        return connected
+
+    async def char_read(self, char_uuid):
+        if self.client == None: return
+        return await self.client.read_gatt_char(char_uuid)
+
+    async def char_write(self, char_uuid, data):
+        if self.client == None: return
+        await self.client.write_gatt_char(char_uuid, data)
+
+
+if __name__ == "__main__":
+    c = ClientHandler()
+    print(TAG, "Revisando las posibles conexiones...")
+    devices = c.discover()
+    print(TAG, "Dispostivos descubiertos:")
+    for i, d in enumerate(devices):
+        print(i, d)
+    
+    i = int(input("Escoge un dispositivo para conectarte: "))
+    selected_mac = devices[i]
+    c.connect(selected_mac)
+    print(TAG, "Conectado a", selected_mac)
+
+    actual_config = c.get_config()
+    print(TAG, "La configuración es", actual_config)
---
+++ codigo_rasp/modelos.py
@@ -0,0 +1,61 @@
+from peewee import *
+
+# Configuración de la base de datos
+db_config = {
+    'host': '0.0.0.0', 
+    'port': 5432, 
+    'user': 'postgres', 
+    'password': 'postgres', 
+    'database': 'iot_db'
+}
+db = PostgresqlDatabase(**db_config)
+
+# Definición de un modelo
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Datos(BaseModel):
+    # HEADER
+    header_id = IntegerField()
+    header_mac = IntegerField()
+    transport_layer = CharField(max_length=3)
+    id_protocol = IntegerField()
+    length = IntegerField()
+
+    # BODY
+    batt_level = IntegerField(null=True)
+    temp = IntegerField(null=True)
+    press = IntegerField(null=True)
+    hum = IntegerField(null=True)
+    co = FloatField(null=True)
+    rms = FloatField(null=True)
+    amp_x = FloatField(null=True)
+    frec_x = FloatField(null=True)
+    amp_y = FloatField(null=True)
+    frec_y = FloatField(null=True)
+    amp_z = FloatField(null=True)
+    frec_z = FloatField(null=True)
+    
+    timestamp = TimestampField()
+    id_device = CharField()
+
+class Logs(BaseModel):
+    id_device = CharField()
+    transport_layer = IntegerField(null=True)
+    id_protocol = IntegerField(null=True)
+    timestamp = DateTimeField()
+
+class Configuration(BaseModel):
+    id_protocol = IntegerField()
+    transport_layer = IntegerField()
+
+class Loss(BaseModel):
+    delay = IntegerField() # ms
+    packet_loss = IntegerField()
+
+
+def create_tables():
+    with db:
+        db.create_tables([Datos, Logs, Configuration, Loss])
+        print("tablas creadas")
